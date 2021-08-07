76cd46c5005144f009e9da2afa68d6ceb3758e79
Andrei Vovchok <andreivovchok@bk.ru>
2021-08-07 19:46:12 +0300

    Добавил книгу и библиотека

---
+++ BooksLibrary/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace BooksLibrary
+{
+    public class Book
+    {
+        private string _title;
+        private string _nameOfTheLibrary;
+        private int _pagesCount;
+
+        public Book(string title, string nameOfTheLibrary, int pagesCount)
+        {
+            _title = title;
+            _nameOfTheLibrary = nameOfTheLibrary;
+            _pagesCount = pagesCount;
+        }
+
+    }
+}
---
+++ BooksLibrary/Library.cs
@@ -0,0 +1,12 @@
+﻿namespace BooksLibrary
+{
+    public class Library
+    {
+        Book[] _books;
+
+        public Library(Book[] books)
+        {
+            _books = books;
+        }
+    }
+}
