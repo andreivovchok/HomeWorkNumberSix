b305b6ad788810291fd53e1398ad779710c014e4
Andrei Vovchok <andreivovchok@bk.ru>
2021-08-10 19:20:28 +0300

    Добавил цикл While

--- BooksConsoleApplication/Program.cs
+++ BooksConsoleApplication/Program.cs
@@ -10,8 +10,8 @@ namespace BooksConsoleApplication
             Book[] books = new Book[]
             {
                 new Book("Война и мир", "Национальная", 1000),
-                new Book("Горе от ума", "Национальная", 200),
-                new Book("1984", "Национальная", 6000),
+                new Book("Горе от ума", "Национальная", 2000),
+                new Book("1984", "Национальная", 600),
             };
 
             Library library = new Library(books);
@@ -20,25 +20,32 @@ namespace BooksConsoleApplication
             Console.WriteLine($"Название библиотеки - {largestBook.NameOfTheLibrary}");
             Console.WriteLine($"Количество страниц - {largestBook.PagesCount}");
 
+            Console.WriteLine();
 
+            Book indexBook = library.GetBookByIndex(0);
+            Console.WriteLine($"Название книги - {indexBook.Title}");
+            Console.WriteLine($"Название библиотеки - {indexBook.NameOfTheLibrary}");
+            Console.WriteLine($"Количество страниц - {indexBook.PagesCount}");
+
+            Console.WriteLine();
 
             Console.WriteLine("Введите название книги!");
-            string bookTitle = Console.ReadLine();
-            Book book = library.GetBookByTitle(bookTitle);
-
-            if (book == null)
+            while (true)
             {
-                Console.WriteLine("Нет такой книги!");
+                string bookTitle = Console.ReadLine();
+                Book book = library.GetBookByTitle(bookTitle);
+                if (book == null)
+                {
+                    Console.WriteLine("Нет такой книги, попробуйте еще раз!");
+                }
+                else
+                {
+                    Console.WriteLine($"Название книги - {book.Title}");
+                    Console.WriteLine($"Название библиотеки - {book.NameOfTheLibrary}");
+                    Console.WriteLine($"Количество страниц - {book.PagesCount}");
+                    break;
+                }
             }
-            else
-            {
-                Console.WriteLine($"Название книги - {book.Title}");
-                Console.WriteLine($"Название библиотеки - {book.NameOfTheLibrary}");
-                Console.WriteLine($"Количество страниц - {book.PagesCount}");
-            }
-
-
-
         }
     }
 }
